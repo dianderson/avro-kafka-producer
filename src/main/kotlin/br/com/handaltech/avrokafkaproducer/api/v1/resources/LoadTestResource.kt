56929975316d100fea1fffcb76310fda315c558f
Dianderson Mendonca <dianderson.mendonca@gmail.com>
2023-03-10 16:59:57 -0300

    insert httpstatus

--- src/main/kotlin/br/com/handaltech/avrokafkaproducer/api/v1/resources/LoadTestResource.kt
+++ src/main/kotlin/br/com/handaltech/avrokafkaproducer/api/v1/resources/LoadTestResource.kt
@@ -6,11 +6,9 @@ import jakarta.validation.Valid
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Validated
 @RestController
@@ -19,6 +17,7 @@ class LoadTestResource(
     private val loadTestPort: LoadTestPort
 ) {
     @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun customerLoadTest(
         @Valid @NotNull @Positive @RequestParam("message-quantity") messageQuantity: Int,
         @Valid @NotNull @Positive @RequestParam("quantity-per-second") quantityPerSecond: Int,
